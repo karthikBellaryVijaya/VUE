5d7c4ef1df7215c68b95a3a4ad058b92be4cd2e2
karthik <kbellaryvijaya@gmail.com>
2019-05-24 18:41:57 -0500

    bundle.js error

---
+++ cutecats/src/main.js
@@ -0,0 +1,16 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
+
+import Vue from 'vue'
+import App from './App.vue'
+new Vue({
+  el: '#app',
+  components: {
+    'app': App
+  }
+})
